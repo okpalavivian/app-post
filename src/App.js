abfd99ef53c05a6c0215b0e227487d21dca0b5a3
okpalavivian <vu.okpala@unizik.edu.ng>
2023-02-12 15:58:43 +0100

    now

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 
-import { Slide } from 'react-slideshow-image';
 import './App.css';
-import Post1 from './pages/Post1';
+
 import Post2 from './pages/Post2';
+import Slideshow from './pages/Slideshow';
 
 function App() {
   return (
     <div className="App">
    
     <Post2 />
-    <Slide />
+  <Slideshow />
     </div>
   );
 }
